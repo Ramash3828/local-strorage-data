08d23c7a94ad4a1c5ea9c6d26400595c580976c4
Ramash3828 <ramash3828@gmail.com>
2022-03-04 17:10:51 +0600

    JS code done

--- app.js
+++ app.js
@@ -1,53 +1,86 @@
-const addToCart = () => {
-    const inputField = document.getElementById("product-name");
-    const name = inputField.value;
-    if (!name) {
+// const addToCart = () => {
+//     const inputField = document.getElementById("product-name");
+//     const name = inputField.value;
+//     if (!name) {
+//         return;
+//     }
+//     // Set item
+//     setItem(name);
+
+//     // Save localStora
+//     addToProduct(name);
+
+//     inputField.value = "";
+// };
+
+// const setItem = (name) => {
+//     const ul = document.getElementById("list");
+//     let li = document.createElement("li");
+//     li.innerText = name;
+//     ul.appendChild(li);
+// };
+
+// const getCart = () => {
+//     const cart = localStorage.getItem("cart");
+
+//     let cartObj;
+//     if (cart) {
+//         cartObj = JSON.parse(cart);
+//     } else {
+//         cartObj = {};
+//     }
+//     return cartObj;
+// };
+
+// const addToProduct = (name) => {
+//     const cart = getCart();
+//     if (cart[name]) {
+//         cart[name] = cart[name] + 1;
+//     } else {
+//         cart[name] = 1;
+//     }
+//     const cartStringified = JSON.stringify(cart);
+//     localStorage.setItem("cart", cartStringified);
+// };
+
+// const getLocalStorageData = () => {
+//     const item = getCart();
+//     for (let name in item) {
+//         setItem(name);
+//     }
+// };
+
+// getLocalStorageData();
+
+const getValue = (ID) => {
+    const value = document.getElementById(ID).value;
+    return value;
+};
+
+const getData = () => {
+    const name = getValue("name");
+    const email = getValue("email");
+    const pass = getValue("pass");
+
+    if (!name || !email || !pass) {
         return;
     }
-    // Set item
-    setItem(name);
+    let user_rec = [];
 
-    // Save localStora
-    addToProduct(name);
-
-    inputField.value = "";
-};
-
-const setItem = (name) => {
-    const ul = document.getElementById("list");
-    let li = document.createElement("li");
-    li.innerText = name;
-    ul.appendChild(li);
-};
-
-const getCart = () => {
-    const cart = localStorage.getItem("cart");
-
-    let cartObj;
-    if (cart) {
-        cartObj = JSON.parse(cart);
+    user_rec = JSON.parse(localStorage.getItem("user"))
+        ? JSON.parse(localStorage.getItem("user"))
+        : [];
+    const checkEmail = user_rec.find((v) => v.email === email);
+    if (checkEmail) {
+        alert("Duplicate Data!!!");
     } else {
-        cartObj = {};
+        user_rec.push({
+            name: name,
+            email: email,
+            pass: pass,
+        });
     }
-    return cartObj;
-};
 
-const addToProduct = (name) => {
-    const cart = getCart();
-    if (cart[name]) {
-        cart[name] = cart[name] + 1;
-    } else {
-        cart[name] = 1;
-    }
-    const cartStringified = JSON.stringify(cart);
-    localStorage.setItem("cart", cartStringified);
+    //
+    localStorage.setItem("user", JSON.stringify(user_rec));
 };
-
-const getLocalStorageData = () => {
-    const item = getCart();
-    for (let name in item) {
-        setItem(name);
-    }
-};
-
-getLocalStorageData();

dfab73aa2c7aa991d85dce7f860ea151252e234b
Ramash3828 <ramash3828@gmail.com>
2022-03-04 13:18:05 +0600

    code done

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const addToCart = () => {
     const inputField = document.getElementById("product-name");
     const name = inputField.value;
+    if (!name) {
+        return;
+    }
     // Set item
     setItem(name);
 
     // Save localStora
+    addToProduct(name);
 
     inputField.value = "";
 };
@@ -15,3 +19,35 @@ const setItem = (name) => {
     li.innerText = name;
     ul.appendChild(li);
 };
+
+const getCart = () => {
+    const cart = localStorage.getItem("cart");
+
+    let cartObj;
+    if (cart) {
+        cartObj = JSON.parse(cart);
+    } else {
+        cartObj = {};
+    }
+    return cartObj;
+};
+
+const addToProduct = (name) => {
+    const cart = getCart();
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    } else {
+        cart[name] = 1;
+    }
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem("cart", cartStringified);
+};
+
+const getLocalStorageData = () => {
+    const item = getCart();
+    for (let name in item) {
+        setItem(name);
+    }
+};
+
+getLocalStorageData();
